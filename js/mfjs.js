8a5f1823a33e9efa3595e3d52ec3dd3d568a50d9
Jaap van Gils <0104106@zadkine.nl>
2022-03-23 12:47:39 +0100

    game verdieping

--- js/mfjs.js
+++ js/mfjs.js
@@ -2,17 +2,17 @@
 // MyFirstJS
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-canvas.fillStyle = "#FF0000";
+canvas.fillStyle = "green";
 canvas.height = canvas.width;
 ctx.transform(1, 0, 0, -1, 0, canvas.height)
 
-let xMax = canvas.height;
+xMax = canvas.height;
 let slope = 1.2;
 let intercept = 70;
 
 ctx.moveTo(0, intercept);
 ctx.lineTo(xMax, f(xMax));
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "red";
 ctx.stroke();
 
 function f(x) {
