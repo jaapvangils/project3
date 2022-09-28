d273ea0c8251a9b0ccee01990cc6a9e23a0fe32c
Jaap van Gils <0104106@zadkine.nl>
2022-09-28 09:50:36 +0200

    toevoegingen

--- python/test.py
+++ python/test.py
@@ -1,5 +1,10 @@
+import os
+os.system("CLS")
+
 uitlezen = input("tik een getal:\n")
 getal = int(uitlezen)
 print("")
 for i in range(10):
-	print(str(i) + ' * ' + str(uitlezen) + ' = ' + str(i*getal))
+	print(str(i+1) + ' * ' + str(uitlezen) + ' = ' + str((i+1)*getal))
+
+	
